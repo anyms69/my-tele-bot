8e71e402224c84ff6d3f811d28620c05aa64dd4a
anyms69 <anym5898@gmail.com>
2026-03-05 18:03:47 +0300

    vite.config.js

--- antalya-miniapp/vite.config.js
+++ antalya-miniapp/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   // Замени 'antalya-miniapp' на название твоего репозитория на GitHub
   // Если репозиторий называется username.github.io — оставь base: '/'
-  base: '/antalya-miniapp/',
+  base: '/anyms69.github.io/my-tele-bot/',
   server: {
     port: 3000,
     host: true
