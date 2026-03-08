67901a703361ee85a6443b9472f940d77c7d7eb6
anyms69 <anym5898@gmail.com>
2026-03-08 08:49:22 +0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-
+window.onerror = function(message, source, lineno, colno, error) {
+  alert('Ошибка: ' + message + '\n' + source + ':' + lineno);
+};
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
